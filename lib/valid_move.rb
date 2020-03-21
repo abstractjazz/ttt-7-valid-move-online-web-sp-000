b08a9c4f988bf44827165dfb84b55a60b5228ea6
Evan Burton <evan@heyevan.com>
2020-03-21 22:08:21 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 def valid_move?(board, index)
-  if position_taken?(board, index) && index.between?(0,8)
-    return nil
+  if !position_taken?(board, index) && index.between?(0,8)
+    return false
   else
     return true
   end
